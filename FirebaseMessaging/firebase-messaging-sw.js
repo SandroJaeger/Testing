ebe095ce10ec261b7e43c59b9d5495aa991cdcbe
SandroJaeger <76667677+SandroJaeger@users.noreply.github.com>
2021-08-11 13:53:14 +0200

    Add files via upload

---
+++ FirebaseMessaging/firebase-messaging-sw.js
@@ -0,0 +1,28 @@
+importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.9.1/firebase-messaging.js');
+
+
+
+var firebaseConfig = {
+
+    apiKey: "AIzaSyDAs-cEPp31JPAmcwx0ikFOxo1lLWThi3g",
+
+    authDomain: "racegame-301b3.firebaseapp.com",
+
+    databaseURL: "https://racegame-301b3.firebaseio.com",
+
+    projectId: "racegame-301b3",
+
+    storageBucket: "racegame-301b3.appspot.com",
+
+    messagingSenderId: "11748553538",
+
+    appId: "1:11748553538:web:2a6788f91386f1158a4edb"
+
+  };
+
+  // Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
